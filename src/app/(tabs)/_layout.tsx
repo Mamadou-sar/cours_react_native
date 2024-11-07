002a085ed7e5c0cb3686e4cad4853473514a63c7
Mamadou Sarr <mamadousarr@MacBook-Pro-de-Mamadou.local>
2024-11-07 02:32:17 +0100

    first commit after create a flatlist

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Tabs } from 'expo-router';
+import { TabBarIcon } from '@/src/components/navigation/TabBarIcon';
+
+export default function TabLayout() {
+   return (
+      <Tabs>
+         <Tabs.Screen
+            name='index'
+            options={{
+               title: 'Home',
+               tabBarIcon: ({ color, focused }) => (
+                  <TabBarIcon
+                     name={focused ? 'home' : 'home-outline'}
+                     color={color}
+                  />
+               ),
+            }}
+         />
+         <Tabs.Screen
+            name='explore'
+            options={{
+               title: 'Explore',
+               tabBarIcon: ({ color, focused }) => (
+                  <TabBarIcon
+                     name={focused ? 'code-slash' : 'code-slash-outline'}
+                     color={color}
+                  />
+               ),
+            }}
+         />
+      </Tabs>
+   );
+}
